3241d20670af8672ea062f4a5ed164b673f9b48b
lastrivia <compositex@sjtu.edu.cn>
2026-03-29 20:39:03 +0800

    batched training

--- physics.py
+++ physics.py
@@ -17,12 +17,14 @@ class CartPoleState:
     v_cart: float
     theta_pole: float
     omega_pole: float
+    is_alive: bool
 
     def __init__(self, x_cart = 0.0, v_cart = 0.0, theta_pole = 0.0, omega_pole = 0.0):
         self.x_cart = x_cart
         self.v_cart = v_cart
         self.theta_pole = theta_pole
         self.omega_pole = omega_pole
+        self.is_alive = self._is_alive()
 
     def to_tensor(self, dtype=torch.float32, device=None) -> torch.Tensor:
         return torch.tensor(
@@ -30,7 +32,7 @@ class CartPoleState:
             dtype=dtype, device=device
         )
 
-    def is_alive(self) -> bool:
+    def _is_alive(self) -> bool:
         return abs(self.theta_pole) < pi / 15.0 and abs(self.x_cart) < 2.4
 
 
@@ -60,4 +62,8 @@ def step(now: CartPoleState, force: float) -> CartPoleState:
     omega_pole += beta_pole * dt
     theta_pole += omega_pole * dt
 
-    return CartPoleState(x_cart, v_cart, theta_pole, omega_pole)
+    next_state = CartPoleState(x_cart, v_cart, theta_pole, omega_pole)
+    if not now.is_alive:
+        next_state.is_alive = False
+
+    return next_state
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import time
 import torch
 from torch.optim import Adam
 
@@ -6,65 +7,88 @@ from model import CartPoleModel
 
 if __name__ == "__main__":
 
+    torch.manual_seed(42)
+
     model = CartPoleModel(hidden=128)
-    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda:0")
     device = torch.device("cpu")
     model.to(device)
 
+    batch_size = 64
     lr = 1e-3
     optimizer = Adam(model.parameters(), lr=lr)
 
-    verbose = 100
+    solved_lifetime = 3600
 
     epoch = 0
+    verbose = 1
+
     sum_lifetime = 0
+    sum_iterations = 0
+    start_time = time.time()
     while True:
         if epoch % verbose == 0 and epoch != 0:
-            print(f"epoch: {epoch}, avg lifetime: {sum_lifetime / verbose}")
-            sum_loss = 0
+            avg_lifetime = sum_lifetime / (verbose * batch_size)
+            elapsed_time = time.time() - start_time
+            start_time = time.time()
+            avg_it_ps = sum_iterations / elapsed_time
+            print(f"epoch: {epoch}, avg lifetime: {avg_lifetime:.1f}, {avg_it_ps:.1f} it/s")
             sum_lifetime = 0
+            sum_iterations = 0
             pass
 
-        state = CartPoleState()
+        states = [CartPoleState() for _ in range(batch_size)]
         log_probs = []
-        rewards = []
+        rewards = [[] for _ in range(batch_size)]
+        masks = [[] for _ in range(batch_size)]
 
-        while True:
-            state_tensor = state.to_tensor(device=device)
+        for _ in range(solved_lifetime):
+            state_tensor = torch.stack([state.to_tensor(device=device) for state in states])
             logits = model(state_tensor)
             probs = torch.softmax(logits, dim=-1)
 
             dist = torch.distributions.Categorical(probs)
-            action = dist.sample()
+            actions = dist.sample()
 
-            log_prob = dist.log_prob(action)
-
-            force = -1.0 if action.item() == 0 else 1.0
-            state = step(state, force)
-
+            log_prob = dist.log_prob(actions)
             log_probs.append(log_prob)
-            if state.is_alive():
-                rewards.append(1.0)
-            else:
-                rewards.append(0.0)
+
+            actions_list = actions.to(torch.device("cpu")).tolist()
+
+            n_alive = 0
+            for i in range(batch_size):
+                masks[i].append(1.0 if states[i].is_alive else 0.0)
+
+                force = -1.0 if actions_list[i] == 0 else 1.0
+                states[i] = step(states[i], force)
+
+                if states[i].is_alive:
+                    n_alive += 1
+                    rewards[i].append(1.0)
+                else:
+                    rewards[i].append(0.0)
+
+            if n_alive == 0:
                 break
 
-        returns = []
-        return_value = 0
-        gamma = 0.99
+        lifetime = [sum(i) for i in masks]
+        sum_lifetime += sum(lifetime)
+        sum_iterations += max(lifetime)
 
-        for r in reversed(rewards):
-            return_value = r + gamma * return_value
-            returns.insert(0, return_value)
+        returns = [[] for _ in range(batch_size)]
+        for i in range(batch_size):
+            return_value = 0
+            gamma = 0.99
+            for r in reversed(rewards[i]):
+                return_value = r + gamma * return_value
+                returns[i].insert(0, return_value)
 
         returns = torch.tensor(returns, device=device)
         returns = (returns - returns.mean()) / (returns.std() + 1e-8)
-        log_probs = torch.stack(log_probs)
+        log_probs = torch.stack(log_probs, dim=-1)
+        masks = torch.tensor(masks, device=device)
 
-        loss = -(log_probs * returns).mean()
-
-        lifetime = len(rewards)
-        sum_lifetime += lifetime
+        loss = -(log_probs * returns * masks).sum() / masks.sum()
 
         optimizer.zero_grad()
         loss.backward()
